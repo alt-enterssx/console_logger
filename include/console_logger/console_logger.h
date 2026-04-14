9b0b6193705392ced9f16724a9dff26b1a81dd47
altenter <altenter.codi@gmail.com>
2026-04-14 07:31:51 +0600

    Add visuals effects

--- include/console_logger/console_logger.h
+++ include/console_logger/console_logger.h
@@ -1,6 +1,27 @@
 #pragma once
 
+#include <iostream>
 #include <string>
+#include <ctime>
+#include <time.h>
+#include <sstream>
+#include <iomanip>
+
+#define WARNING_COLOR "\033[1;30;43m"
+#define DEBUG_COLOR "\033[1;30;105m"
+#define INFO_COLOR "\033[1;30;102m"
+#define ERROR_COLOR "\033[1;37;101m"
+#define CRITICAL_COLOR "\033[1;37;41m"
+
+#define WARNING_COLOR_TEXT "\033[0;33m"
+#define DEBUG_COLOR_TEXT "\033[0;95m"
+#define INFO_COLOR_TEXT "\033[0;92m"
+#define ERROR_COLOR_TEXT "\033[0;91m"
+#define CRITICAL_COLOR_TEXT "\033[0;31m"
+
+#define TIME_COLOR "\033[1;37;40m"
+
+#define RESET "\033[0m"
 
 namespace altenter {
 
@@ -17,9 +38,12 @@ namespace altenter {
             ConsoleLogger();
             ~ConsoleLogger();
 
-            void log(const char* msg, altenter::LogType type = altenter::LogType::DEBUG);
-            void log(const std::string& msg, altenter::LogType type = altenter::LogType::DEBUG);
+            void log(const std::string&& msg, altenter::LogType type = altenter::LogType::DEBUG);
 
         private:
+            std::string getPrefix(altenter::LogType type);
+            std::string getTimeInfo();
+            char* getColorText(altenter::LogType type);
+            char* getColorBg(altenter::LogType type);
     };
 }
--- src/console_logger.cpp
+++ src/console_logger.cpp
@@ -1,7 +1,110 @@
-#include "console_logger.h"
+#include "console_logger/console_logger.h"
 
 altenter::ConsoleLogger::ConsoleLogger() {}
 altenter::ConsoleLogger::~ConsoleLogger() {}
 
-void altenter::ConsoleLogger::log(const char* msg, altenter::LogType type) {}
-void altenter::ConsoleLogger::log(const std::string& msg, altenter::LogType type) {}
+
+void altenter::ConsoleLogger::log(const std::string&& msg, altenter::LogType type) {
+    std::string prefix = this->getPrefix(type);
+    std::string time_str = this->getTimeInfo();
+    std::cout << prefix << RESET << ' ' << TIME_COLOR << ' ' << time_str << ' ' 
+        << RESET << ' ' << this->getColorBg(type) << ' ' << RESET
+        << ' ' << this->getColorText(type) << msg << RESET << std::endl;
+}
+
+std::string altenter::ConsoleLogger::getPrefix(altenter::LogType type) {
+    std::string combined;
+    switch(type) {
+        case altenter::LogType::WARGNING: {
+            combined = WARNING_COLOR;
+            combined = combined + " | WARNING  ";
+
+            break;
+        }
+        case altenter::LogType::DEBUG: {
+            combined = DEBUG_COLOR;
+            combined = combined + " | DEBUG    ";
+
+            break;
+        }
+        case altenter::LogType::INFO: {
+            combined = INFO_COLOR;
+            combined = combined + " | INFO     ";
+
+            break;
+        }
+        case altenter::LogType::ERROR: {
+            combined = ERROR_COLOR;
+            combined = combined + " | ERROR    ";
+
+            break;
+        }
+        case altenter::LogType::CRITICAL: {
+            combined = CRITICAL_COLOR;
+            combined = combined + " | CRITICAL ";
+
+            break;
+        }
+        default: { return ""; }
+    }
+
+    return combined;
+}
+
+std::string altenter::ConsoleLogger::getTimeInfo() {
+    std::time_t t = time(nullptr);
+    std::tm* loctime = localtime(&t);
+
+    std::stringstream ss;
+    ss << loctime->tm_year + 1900 << '.'
+        << std::setw(2) << std::setfill('0') << loctime->tm_mday << '.'
+        << std::setw(2) << std::setfill('0') << loctime->tm_mon + 1 << '.' << ' '
+        << std::setw(2) << std::setfill('0') << loctime->tm_hour << ':'
+        << std::setw(2) << std::setfill('0') << loctime->tm_min << ':'
+        << std::setw(2) << std::setfill('0') << loctime->tm_sec;
+
+    std::string time_str = ss.str();
+    return time_str;
+}
+
+char* altenter::ConsoleLogger::getColorText(altenter::LogType type) {
+    switch(type) {
+        case altenter::LogType::WARGNING: {
+            return WARNING_COLOR_TEXT;
+        }
+        case altenter::LogType::DEBUG: {
+            return DEBUG_COLOR_TEXT;
+        }
+        case altenter::LogType::INFO: {
+            return INFO_COLOR_TEXT;
+        }
+        case altenter::LogType::ERROR: {
+            return ERROR_COLOR_TEXT;
+        }
+        case altenter::LogType::CRITICAL: {
+            return CRITICAL_COLOR_TEXT;
+        }
+        default: { return ""; }
+    }
+}
+
+char* altenter::ConsoleLogger::getColorBg(altenter::LogType type) {
+    switch(type) {
+        case altenter::LogType::WARGNING: {
+            return WARNING_COLOR;
+        }
+        case altenter::LogType::DEBUG: {
+            return DEBUG_COLOR;
+        }
+        case altenter::LogType::INFO: {
+            return INFO_COLOR;
+        }
+        case altenter::LogType::ERROR: {
+            return ERROR_COLOR;
+        }
+        case altenter::LogType::CRITICAL: {
+            return CRITICAL_COLOR;
+        }
+        default: { return ""; }
+    }
+}
